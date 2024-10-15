ba350f237be2d0fb9368d19daf357ed555a99ddc
JotBleach <aendal522@gmail.com>
2024-10-15 15:31:40 -0500

    updated tab logo

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
                 <title>Udaan</title>
                 <meta name='description' content='Blinder making it simple for you to build and grow your SaaS applications, or any business idea' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <link rel='icon' href='/favicon.ico' />
+                <link rel='icon' href='/UdaanLogo.png' />
             </Head>
             <main>{children}</main>
             <Footer />
